dbbde803bcde02f7830d50b88ce343a174a9a088
Indracit S <indrajithij802@gmail.com>
2022-05-04 23:35:06 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -4,8 +4,7 @@ const { MongoClient } = require('mongodb');
 
 async function main(callback) {
     const URI =
-      process.env.MONGO_URI ||
-      'mongodb+srv://ind_98:Siva9751@cluster0.8zdgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' // Declare MONGO_URI in your .env file
+      process.env.MONGO_URI // Declare MONGO_URI in your .env file
     const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
