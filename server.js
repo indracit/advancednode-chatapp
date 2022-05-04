dc6be00fdc857689313ecaf59b82b26d8adad329
Indracit S <indrajithij802@gmail.com>
2022-05-04 18:14:19 +0530

    make it better

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-'use strict';
+'use strict'
 const routes = require('./routes.js')
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+require('dotenv').config()
+const express = require('express')
+const myDB = require('./connection')
+const fccTesting = require('./freeCodeCamp/fcctesting.js')
 const session = require('express-session')
 const passport = require('passport')
 const ObjectID = require('mongodb').ObjectID
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcrypt')
 const auth = require('./auth.js')
-const app = express();
+const app = express()
 
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
 app.set('view engine', 'pug')
-fccTesting(app); 
+fccTesting(app)
 
-
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(
   session({
     secret: process.env.SESSION_SECRET || '12345',
@@ -33,41 +32,35 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-
-
 myDB(async (client) => {
   const myDataBase = await client.db('urlshortner').collection('userdata')
 
   routes(app, myDataBase)
-  auth(app, myDataBase) 
+  auth(app, myDataBase)
 
-  
-  let currentUsers = 0 ;
+  let currentUsers = 0
 
   io.on('connection', (socket) => {
-    ++currentUsers;
-    io.emit('user count', currentUsers);
-    console.log('A user has connected');
+    ++currentUsers
+    io.emit('user count', currentUsers)
+    console.log('A user has connected')
   })
 
   io.on('disconnect', () => {
     console.log('A user has disconnected')
-    --currentUsers;
-    io.emit('user count', currentUsers);
+    --currentUsers
+    io.emit('user count', currentUsers)
   })
-}).catch(e => {
+}).catch((e) => {
   app.route('/').get((req, res) => {
     res.render(process.cwd() + '/views/pug', {
       title: e,
       message: 'Unable to login',
     })
-  });
-});
+  })
+})
 
-
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
+  console.log('Listening on port ' + PORT)
+})
